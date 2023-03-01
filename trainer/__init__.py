1f65e1475d9a4aa0adf07dea64fa64bc6e3f72a1
Saul Alonso Monsalve <saul.alonso.monsalve@cern.ch>
2023-03-01 16:34:59 +0100

    minor change

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,4 +1,4 @@
 from .optim_schedule import ScheduledOptim
 from .loss import Chi2Loss, PoissonLikelihood_loss, Combined2Losses, Combined3Losses
-from .plot import plot_event
-from .train import generator_train_step, generator_test_step
+from .plot import plot_event, plot_scatter, plot_hist, plot_scatter_len
+from .train import generator_train_step, generator_test_step, set_seed
